5b3ccb9eb3a04bbe012816955aafcdfddf8745d5
Gitau Karugi <gkarugi@gmail.com>
2023-05-29 20:37:11 +0300

    404 page

--- source/404.blade.php
+++ source/404.blade.php
@@ -1,14 +1,22 @@
 @extends('_layouts.master')
 
 @section('body')
-    <div class="d-flex align-items-center justify-content-center vh-100">
-        <div class="text-center">
-            <h1 class="display-1 fw-bold">404</h1>
-            <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
-            <p class="lead">
-                The page you’re looking for does not exist.
-            </p>
-            <a href="/" class="btn btn-primary">Go Home</a>
-        </div>
-    </div>
+    <section class="section nft-hero" id="hero">
+        <div class="bg-overlay"></div>
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-sm-10">
+                    <div class="text-center">
+                        <h1 class="display-4 fw-semibold mb-4 lh-base text-white">404 </h1>
+                        <h3 class="text-white"><span class="text-danger">Oopps!</span> Page not found</h3>
+                        <p class="lead text-white lh-base my-4 pb-2">The page you’re looking for does not exist.</p>
+
+                        <div class="hstack gap-2 justify-content-center">
+                            <a href="/" class="btn btn-primary"><i class="ri-arrow-left-line align-middle ms-1"></i> Go Home</a>
+                        </div>
+                    </div>
+                </div><!--end col-->
+            </div><!-- end row -->
+        </div><!-- end container -->
+    </section>
 @endsection
--- source/_partials/nav.blade.php
+++ source/_partials/nav.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-landing navbar-light fixed-top" id="navbar">
     <div class="container">
-        <a class="navbar-brand" href="index.html">
-            <img src="assets/images/motordeft-logo.png" class="card-logo card-logo-dark" alt="logo dark" width="150">
-            <img src="assets/images/motordeft-logo.png" class="card-logo card-logo-light" alt="logo light" width="150">
+        <a class="navbar-brand" href="/">
+            <img src="/assets/images/motordeft-logo.png" class="card-logo card-logo-dark" alt="logo dark" width="150">
+            <img src="/assets/images/motordeft-logo.png" class="card-logo card-logo-light" alt="logo light" width="150">
         </a>
         <button class="navbar-toggler py-0 fs-20 text-body" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <i class="mdi mdi-menu"></i>
